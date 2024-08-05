af1a97d5af94edf41d8b83c4357463209792fcb5
Suryansh Chauhan <suryanshchauhan_2023@depauw.edu>
2024-08-05 12:40:40 -0400

    changes

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,16 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD5YUpSpmJXHDvkAnrN_UgdcSYx6NyctJo",
+  authDomain: "ipantry-bf023.firebaseapp.com",
+  projectId: "ipantry-bf023",
+  storageBucket: "ipantry-bf023.appspot.com",
+  messagingSenderId: "375392292154",
+  appId: "1:375392292154:web:f3d3a166b43299a9b6d403",
+  measurementId: "G-P1GQLY652Y"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
